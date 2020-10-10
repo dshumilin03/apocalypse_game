1ed5b37f2bcf8b61779351fcaebde3eb312f2061
dshumilin03 <dshumilin04@gmail.com>
2020-10-10 22:13:07 +0300

    +mouse 1 shooting + a d movement +settings which includes regulation of volume +code is structured

--- proekt.py
+++ proekt.py
@@ -1,30 +1,42 @@
+# <editor-fold desc="Imports">
 import pygame
 import random
 import sys
 from os import path
 import time
-#var
+
+# </editor-fold>
+
+# <editor-fold desc="vars">
 won = False
 about = False
 paused = False
 dead = False
 intro = True
+gear = False
+target = False
 sy = 1
 sx = 3
 pts = 0
+volume = 0
 need = 100
 shootlimit = 9
 meteors = 8
-#fonts
+# </editor-fold>
+
+# <editor-fold desc="Fonts">
 pygame.font.init()
 smalltext = pygame.font.SysFont("comicsansms", 25)
 mediumtext = pygame.font.SysFont("comicsansms", 50)
 largetext = pygame.font.SysFont("comicsansms", 75)
-#screen
+# </editor-fold>
+
+# <editor-fold desc="Screen">
 img_dir = path.join(path.dirname(__file__), 'img')
 WIDTH = 480
 HEIGHT = 600
 FPS = 60
+# </editor-fold>
 
 # <editor-fold desc="colors">
 WHITE = (255, 255, 255)
@@ -38,24 +50,29 @@ YELLOW = (255, 255, 0)
 LIGHT_BLUE = (0, 160, 255)
 # </editor-fold>
 
-# initialize pygame and create game window
+# <editor-fold desc="initialize pygame and create game window">
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Apocalypse")
 clock = pygame.time.Clock()
-#sounds
+# </editor-fold>
+
+# <editor-fold desc="Sounds">
 crash_sound = pygame.mixer.Sound("crash.wav")
-crash_sound.set_volume(0.08)
-pygame.mixer.music.load("march.mp3")
-pygame.mixer.music.set_volume(0.2)
+crash_sound.set_volume(0.08 * (0.01*volume))
+pygame.mixer.music.load("purpleheart.mp3")
+pygame.mixer.music.set_volume(0.2 * (0.01*volume))
 laser_sound = pygame.mixer.Sound("laser2.wav")
-laser_sound.set_volume(0.2)
+laser_sound.set_volume(0.1 * (0.01*volume))
 pygame.mixer.music.play(-1)
 player_crashed = pygame.mixer.Sound("player_crash.wav")
-player_crashed.set_volume(0.3)
-#menu
+player_crashed.set_volume(0.3 * (0.01*volume))
+# </editor-fold>
+
+
 def menu():
+    global gear
     global about
     global dead
     global intro
@@ -64,7 +81,7 @@ def menu():
     intro = True
     while intro:
         for event in pygame.event.get():
-            #print(event)
+            # print(event)
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
@@ -73,28 +90,39 @@ def menu():
         menu_text = mediumtext.render("Apocalypse", True, LIGHT_BLUE)
         # pygame.draw.rect(screen, GREEN, (180, 155, 122, 45))
         # pygame.draw.rect(screen, RED, (170, 245, 145, 55))
-        #pygame.draw.rect(screen, YELLOW, (155, 500, 183, 55))
+        # pygame.draw.rect(screen, YELLOW, (155, 500, 183, 55))
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
         #print(mouse)
+
         if 302 >= mouse[0] >= 180 and 200 >= mouse[1] >= 155:
             Play = mediumtext.render("PLAY", True, BLUE)
             screen.blit(Play, [180, 140])
         else:
             Play = mediumtext.render("PLAY", True, LIGHT_BLUE)
             screen.blit(Play, [180, 140])
-        if 315 >= mouse[0] >= 170 and 300 >= mouse[1] >= 245:
-            Quit = mediumtext.render("QUIT", True, BLUE)
-            screen.blit(Quit, [170, 230])
+
+        if 380 >= mouse[0] >= 120 and 285 >= mouse[1] >= 245:
+            gears = mediumtext.render("SETTINGS", True, BLUE)
+            screen.blit(gears, [115, 230])
         else:
-            Quit = mediumtext.render("QUIT", True, LIGHT_BLUE)
-            screen.blit(Quit, [170, 230])
+            gears = mediumtext.render("SETTINGS", True, LIGHT_BLUE)
+            screen.blit(gears, [115, 230])
+
         if 335 >= mouse[0] >= 155 and 547 >= mouse[1] >= 507:
             About = mediumtext.render("ABOUT", True, BLUE)
             screen.blit(About, [155, 490])
         else:
             About = mediumtext.render("ABOUT", True, LIGHT_BLUE)
             screen.blit(About, [155, 490])
+        screen.blit(menu_text, [110, 0])
+
+        if 315 >= mouse[0] >= 170 and 377 >= mouse[1] >= 340:
+            quit = mediumtext.render("QUIT", True, BLUE)
+            screen.blit(quit, [170, 320])
+        else:
+            quit = mediumtext.render("QUIT", True, LIGHT_BLUE)
+            screen.blit(quit, [170, 320])
         screen.blit(menu_text, [110, 0])
 
         if click[0] == 1 and 302 >= mouse[0] >= 180 and 200 >= mouse[1] >= 155:
@@ -111,7 +139,7 @@ def menu():
             pts = 0
             running = True
 
-        elif click[0] == 1 and 315 >= mouse[0] >= 170 and 300 >= mouse[1] >= 245:
+        elif click[0] == 1 and 315 >= mouse[0] >= 170 and 377 >= mouse[1] >= 340:
             pygame.quit()
             quit()
         else:
@@ -121,8 +149,15 @@ def menu():
         if click[0] == 1 and 335 >= mouse[0] >= 155 and 547 >= mouse[1] >= 507:
             about = True
             description()
+
+        if click[0] == 1 and 380 >= mouse[0] >= 120 and 285 >= mouse[1] >= 245:
+            gear = True
+            settings()
+
         pygame.display.update()
         clock.tick(15)
+
+
 def died():
     global sx
     global sy
@@ -133,9 +168,9 @@ def died():
     timer = 0
     while dead:
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
         screen.blit(game_over_img, game_over_img_rect)
         killed = largetext.render("YOU DIED", True, RED)
         screen.blit(killed, [55, 100])
@@ -144,6 +179,7 @@ def died():
         timer += 1
         if timer == 45:
             menu()
+
 
 def pause():
     global paused
@@ -164,53 +200,124 @@ def pause():
         pygame.display.update()
         clock.tick(15)
 
+
 def score(pts):
-    pts_text = smalltext.render("Score: "+str(pts), True, YELLOW)
+    pts_text = smalltext.render("Score: " + str(pts), True, YELLOW)
     screen.blit(pts_text, [0, 0])
+
+
+def settings():
+    global gear
+    global volume
+    global target
+    while gear:
+        volume0_pos = 140
+        volume100_pos = 340
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        screen.fill(BLACK)
+
+        if 307 >= mouse[0] >= 157 and 457 >= mouse[1] >= 417:
+            About = mediumtext.render("MENU", True, BLUE)
+            screen.blit(About, [155, 400])
+        else:
+            About = mediumtext.render("MENU", True, LIGHT_BLUE)
+            screen.blit(About, [155, 400])
+
+        sound_text = mediumtext.render("VOLUME", True, LIGHT_BLUE)
+
+        screen.blit(sound_text, [130, 25])
+
+        pygame.draw.rect(screen, WHITE, ((140, 180), (200, 3)))
+
+        if volume100_pos >= mouse[0] >= volume0_pos and 170 <= mouse[1] <= 190 and click[0] == 1:
+            target = True
+        elif click[0] == 0:
+            target = False
+
+        if target and volume100_pos >= mouse[0] >= volume0_pos:
+            pygame.draw.circle(screen, WHITE, (mouse[0], 180), 10)
+            volume = (mouse[0] - volume0_pos) // 2
+
+        elif target and mouse[0] < volume0_pos:
+            pygame.draw.circle(screen, WHITE, (volume0_pos, 180), 10)
+            volume = 0
+        elif target and mouse[0] > volume100_pos:
+            pygame.draw.circle(screen, WHITE, (volume100_pos, 180), 10)
+            volume = 100
+        else:
+            pygame.draw.circle(screen, WHITE, (volume * 2 + volume0_pos, 180), 10)
+
+        if 100 > volume >= 10:
+            volume_percentage = mediumtext.render(" " + str(volume) + "%", True, LIGHT_BLUE)
+
+        elif volume < 10:
+            volume_percentage = mediumtext.render("  " + str(volume) + "%", True, LIGHT_BLUE)
+
+        elif volume == 100:
+            volume_percentage = mediumtext.render(str(volume) + "%", True, LIGHT_BLUE)
+
+        screen.blit(volume_percentage, [180, 85])
+
+        crash_sound.set_volume(0.08 * (0.01 * volume))
+        player_crashed.set_volume(0.3 * (0.01 * volume))
+        laser_sound.set_volume(0.2 * (0.01 * volume))
+        pygame.mixer.music.set_volume(0.2 * (0.01 * volume))
+
+        if click[0] == 1 and 335 >= mouse[0] >= 155 and 457 >= mouse[1] >= 417:
+            gear = False
+            menu()
+
+        pygame.display.update()
+        clock.tick(60)
 
 
 def description():
     global about
     while about:
-         mouse = pygame.mouse.get_pos()
-         click = pygame.mouse.get_pressed()
-         for event in pygame.event.get():
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-         screen.blit(about_img, about_img_rect)
-         #print(mouse)
-         #pygame.draw.rect(screen, YELLOW, (157, 413, 150, 49))
+        screen.blit(about_img, about_img_rect)
+        # print(mouse)
+        # pygame.draw.rect(screen, YELLOW, (157, 413, 150, 49))
 
-         #one line is about 42 symbols
-         about_text = smalltext.render("This game is about space ship that came", True, LIGHT_BLUE)
-         screen.blit(about_text, [4, 5])
+        # one line is about 42 symbols
+        about_text = smalltext.render("This game is about space ship that came", True, LIGHT_BLUE)
+        screen.blit(about_text, [4, 5])
 
-         about_text1 = smalltext.render("to the meteor rain.", True, LIGHT_BLUE)
-         screen.blit(about_text1, [4, 35])
+        about_text1 = smalltext.render("to the meteor rain.", True, LIGHT_BLUE)
+        screen.blit(about_text1, [4, 35])
 
-         about_text2 = smalltext.render("To shoot, you must press Space.", True, LIGHT_BLUE)
-         screen.blit(about_text2, [4, 85])
+        about_text2 = smalltext.render("To shoot, you must press Space.", True, LIGHT_BLUE)
+        screen.blit(about_text2, [4, 85])
 
-         about_text4 = smalltext.render("Arrows to move left/right.", True, LIGHT_BLUE)
-         screen.blit(about_text4, [4, 185])
+        about_text4 = smalltext.render("Arrows to move left/right.", True, LIGHT_BLUE)
+        screen.blit(about_text4, [4, 185])
 
-         about_text5 = smalltext.render("You can pause the game by clicking Esc", True, LIGHT_BLUE)
-         screen.blit(about_text5, [4, 225])
-         #
-         if 307 >= mouse[0] >= 157 and 457 >= mouse[1] >= 417:
+        about_text5 = smalltext.render("You can pause the game by clicking Esc", True, LIGHT_BLUE)
+        screen.blit(about_text5, [4, 225])
+        #
+        if 307 >= mouse[0] >= 157 and 457 >= mouse[1] >= 417:
             About = mediumtext.render("MENU", True, BLUE)
             screen.blit(About, [155, 400])
-         else:
+        else:
             About = mediumtext.render("MENU", True, LIGHT_BLUE)
             screen.blit(About, [155, 400])
 
-         if click[0] == 1 and 335 >= mouse[0] >= 155 and 457 >= mouse[1] >= 417:
+        if click[0] == 1 and 335 >= mouse[0] >= 155 and 457 >= mouse[1] >= 417:
             about = False
             menu()
-         #
-         pygame.display.update()
-         clock.tick(15)
+        #
+        pygame.display.update()
+        clock.tick(60)
 
 
 class Player(pygame.sprite.Sprite):
@@ -221,27 +328,30 @@ class Player(pygame.sprite.Sprite):
         self.image.set_colorkey(WHITE)
         self.rect = self.image.get_rect()
         self.radius = 21
-        #pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
-        #hitboxes CBEPXY
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        # hitboxes CBEPXY
         self.rect.centerx = WIDTH // 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
+
     def update(self):
         self.speedx = 0
         keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_LEFT]:
+        if keystate[pygame.K_LEFT] or keystate[pygame.K_a]:
             self.speedx = -5
-        if keystate[pygame.K_RIGHT]:
+        if keystate[pygame.K_RIGHT] or keystate[pygame.K_d]:
             self.speedx = 5
         self.rect.x += self.speedx
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
+
     def shoot(self):
-          bullet = Bullet(self.rect.centerx, self.rect.top)
-          all_sprites.add(bullet)
-          bullets.add(bullet)
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
 
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
@@ -249,23 +359,25 @@ class Mob(pygame.sprite.Sprite):
         self.image = meteor_img
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.radius = int(self.rect.width*.85 / 2)
-        #pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
-        #hitboxes CBEPXY
+        self.radius = int(self.rect.width * .85 / 2)
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        # hitboxes CBEPXY
         self.rect.x = random.randrange(WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
-        self.speedy = random.randrange(sx, sy+7)
-        self.speedx = random.randrange(sx-6, sy+2)
+        self.speedy = random.randrange(sx, sy + 7)
+        self.speedx = random.randrange(sx - 6, sy + 2)
         self.rot = 0
         self.rot_speed = random.randrange(-8, 8)
         self.last_update = pygame.time.get_ticks()
+
     def update(self):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
         if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
             self.rect.x = random.randrange(WIDTH - self.rect.width)
             self.rect.y = random.randrange(-100, -40)
-            self.speedy = random.randrange(sx, sy+7)
+            self.speedy = random.randrange(sx, sy + 7)
+
 
 class Explosion(pygame.sprite.Sprite):
     def __init__(self, center, size):
@@ -291,6 +403,7 @@ class Explosion(pygame.sprite.Sprite):
                 self.rect = self.image.get_rect()
                 self.rect.center = center
 
+
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -300,12 +413,15 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.bottom = y
         self.rect.centerx = x
         self.speedy = -10
+
     def update(self):
         self.rect.y += self.speedy
         # kill if it moves out from screen
         if self.rect.bottom < 0:
             self.kill()
-# Load all game graphics
+
+
+# <editor-fold desc="Load all game graphics">
 background = pygame.image.load(path.join(img_dir, "starfield.png")).convert()
 menu_background = pygame.image.load(path.join(img_dir, "71.jpg")).convert()
 player_img = pygame.image.load(path.join(img_dir, "playerShip1_orange.png")).convert()
@@ -320,6 +436,8 @@ background_rect = background.get_rect()
 explosion_anim = {}
 explosion_anim["lg"] = []
 explosion_anim["sm"] = []
+# </editor-fold>
+
 for i in range(9):
     filename = "regularExplosion0{}.png".format(i)
     img = pygame.image.load(filename).convert()
@@ -328,30 +446,42 @@ for i in range(9):
     explosion_anim["lg"].append(img_lg)
     img_sm = pygame.transform.scale(img, (32, 32))
     explosion_anim["sm"].append(img_sm)
-#
+
+# <editor-fold desc="Working with sprites">
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
+bullets = pygame.sprite.Group()
+
+
+# </editor-fold>
+
+
 def spawn():
     for i in range(meteors):
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
-spawn()
 
-bullets = pygame.sprite.Group()
-# Game loop
+
+spawn()
 menu()
+
 while running:
-    click = pygame.mouse.get_pressed()
     # keep loop running at the right speed
     clock.tick(FPS)
     # Process input (events)
     for event in pygame.event.get():
+        click = pygame.mouse.get_pressed()
         # check for game over
         if event.type == pygame.QUIT:
             running = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if click[0] == 1:
+                player.shoot()
+
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and len(bullets) < shootlimit:  # shooting here
                 player.shoot()
@@ -359,7 +489,6 @@ while running:
 
             if event.key == pygame.K_ESCAPE:
                 pause()
-            print(pygame.mouse.get_pressed()[0])
 
     if 100 > pts >= 50:
         shootlimit = 8
@@ -406,7 +535,7 @@ while running:
         running = False
         time.sleep(1)
         died()
-        #menu()
+        # menu()
 
     # Draw and render
     screen.blit(background, background_rect)
@@ -414,16 +543,15 @@ while running:
     score(pts)
     # helpful thing
     pygame.display.flip()
-pygame.quit()
 
-#1) Новый интерфейс
-#2) Настройки(Бинды(мышка, пробел), громкость и подобные)
-#3) Различные Корабли с другими оружиями
-#4) Фикс сцены смерти  ✔
-#5) Убрать лимит очков  ✔
-#6) Пересмотреть систему повышения сложности
-#7) Добавить крутящий момент метеоритам
-#8) Убрать бомбу из игры  ✔
-#9) Добавить возможность при паузе выходить в меню и выключать игру
-#10) Оптимизация алгоритмов
-#11) Структурирование кода
+# 1) Новый интерфейс
+# 2) Настройки(Бинды(мышка, пробел), громкость и подобные)
+# 3) Различные Корабли с другими оружиями
+# 4) Фикс сцены смерти  ✔
+# 5) Убрать лимит очков  ✔
+# 6) Пересмотреть систему повышения сложности
+# 7) Добавить крутящий момент метеоритам
+# 8) Убрать бомбу из игры  ✔
+# 9) Добавить возможность при паузе выходить в меню и выключать игру
+# 10) Оптимизация алгоритмов
+# 11) Структурирование кода ✔
